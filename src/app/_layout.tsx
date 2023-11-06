acc0893528bf34edbcfd9796e85ebb572e519d3f
Gromzi <mamgier@gmail.com>
2023-11-06 13:46:53 +0100

    Style bottom navigation bar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,41 +1,59 @@
-import { SplashScreen, Stack } from "expo-router";
-import { AppAuthInitializer } from "../utils/AppAuthInitializer";
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
+import { SplashScreen, Stack } from 'expo-router'
+import { AppAuthInitializer } from '../utils/AppAuthInitializer'
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react'
 import {
   ThemeProvider,
   DarkTheme,
   DefaultTheme,
-} from "@react-navigation/native";
-import { useColorScheme } from "react-native";
+} from '@react-navigation/native'
+import { useColorScheme } from 'react-native'
+import * as NavigationBar from 'expo-navigation-bar'
+import { colorPalette } from '../theme/colors'
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default () => {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
+
+  if (colorScheme === 'dark') {
+    NavigationBar.setBackgroundColorAsync(colorPalette.dark.contrast)
+    NavigationBar.setButtonStyleAsync('light')
+  } else {
+    NavigationBar.setBackgroundColorAsync(colorPalette.light.contrast)
+    NavigationBar.setButtonStyleAsync('dark')
+  }
 
   const [fontsLoaded, fontError] = useFonts({
-    "Kanit-Regular": require("../../assets/fonts/Kanit-Regular.ttf"),
-    "Kanit-Bold": require("../../assets/fonts/Kanit-Bold.ttf"),
-    "Kanit-Medium": require("../../assets/fonts/Kanit-Medium.ttf"),
-  });
+    'Kanit-Regular': require('../../assets/fonts/Kanit-Regular.ttf'),
+    'Kanit-Bold': require('../../assets/fonts/Kanit-Bold.ttf'),
+    'Kanit-Medium': require('../../assets/fonts/Kanit-Medium.ttf'),
+  })
 
   useEffect(() => {
     if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync();
+      SplashScreen.hideAsync()
     }
-  }, [fontsLoaded, fontError]);
+  }, [fontsLoaded, fontError])
 
-  if (!fontsLoaded || fontError) return null;
+  if (!fontsLoaded || fontError) return null
 
   return (
     <AppAuthInitializer>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <ThemeProvider
+        value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+      >
         <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="index"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="(tabs)"
+            options={{ headerShown: false }}
+          />
         </Stack>
       </ThemeProvider>
     </AppAuthInitializer>
-  );
-};
+  )
+}
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,14 +1,16 @@
 const colorPalette = {
-    dark: {
-        primary: "#8B1874",
-        secondary: "#F79540",
-        text: "#FCECDD"
-    },
-    light: {
-        primary: "#F79540",
-        secondary: "#8B1874",
-        text: "#FCECDD"
-    },
+  dark: {
+    primary: '#8B1874',
+    secondary: '#F79540',
+    contrast: '#2F3033',
+    text: '#FCECDD',
+  },
+  light: {
+    primary: '#F79540',
+    secondary: '#8B1874',
+    contrast: '#FCECDD',
+    text: '#FCECDD',
+  },
 }
 
 export { colorPalette }
